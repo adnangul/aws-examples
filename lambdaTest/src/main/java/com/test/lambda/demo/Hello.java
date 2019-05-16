59df68b0c5ac6e780d698463041170eaf2144846
Adnan Yaqoob <itsadnan@gmail.com>
2019-05-16 16:52:57 -0500

    my first java based lambda test

---
+++ lambdaTest/src/main/java/com/test/lambda/demo/Hello.java
@@ -0,0 +1,17 @@
+package com.test.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, String> {
+
+	@Override
+	public String handleRequest(Object input, Context context) {
+		// input - "Adnan"
+		context.getLogger().log("Input: " + input);
+		
+		String output = "Hello, " + input + "!";
+		return output;
+	}
+
+}
---
+++ lambdaTest/src/test/java/com/test/lambda/demo/HelloTest.java
@@ -0,0 +1,43 @@
+package com.test.lambda.demo;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class HelloTest {
+
+    private static Object input;
+
+    @BeforeClass
+    public static void createInput() throws IOException {
+        // TODO: set up your sample input object here.
+        input = "Adnan";
+    }
+
+    private Context createContext() {
+        TestContext ctx = new TestContext();
+
+        // TODO: customize your context here if needed.
+        ctx.setFunctionName("HelloFunction");
+
+        return ctx;
+    }
+
+    @Test
+    public void testHello() {
+        Hello handler = new Hello();
+        Context ctx = createContext();
+
+        String output = handler.handleRequest(input, ctx);
+
+        // TODO: validate output here if needed.
+        Assert.assertEquals("Hello, Adnan!", output);
+    }
+}
